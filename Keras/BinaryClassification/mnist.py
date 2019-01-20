88b82d9885a5892bd653737694165d26e304df4f
Nader Zare <nader.zare88@gmail.com>
2019-01-20 17:47:51 +0330

    add mnist

---
+++ Keras/BinaryClassification/mnist.py
@@ -0,0 +1,57 @@
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras import models
+from keras import layers
+import matplotlib.pyplot as plt
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+#show one of train image
+digit = train_images[4]
+plt.imshow(digit, cmap=plt.cm.binary)
+# plt.show()
+
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+network = models.Sequential()
+network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
+network.add(layers.Dense(10, activation='softmax'))
+network.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+history = network.fit(train_images, train_labels, epochs=2, batch_size=32, validation_data=(test_images, test_labels))
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+
+# dict_keys(['val_loss', 'val_acc', 'loss', 'acc'])
+history_dict = history.history
+print(history_dict['val_loss'])
+print(history_dict['val_acc'])
+print(history_dict['loss'])
+print(history_dict['acc'])
+epochs = range(1, len(history_dict['acc']) + 1)
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+plt.clf()
+plt.figure(1)
+plt.subplot(211)
+plt.plot(epochs, loss_values, 'r--', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b--', label='Validation loss')
+plt.title("train/test loss")
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.subplot(212)
+plt.plot(epochs, acc_values, 'r--', label='Training acc')
+plt.plot(epochs, val_acc_values, '--', label='Validation acc')
+plt.title("train/test acc")
+plt.xlabel('Epochs')
+plt.ylabel('Acc')
+plt.legend()
+plt.show()
--- numpytest.py
+++ numpytest.py
@@ -1,48 +1,54 @@
 import numpy as np
-
-x = np.array(12)
-print (x,x.ndim,x.shape)
-print(len(x.shape))
-
-x = np.array([1,2,3])
-print (x,x.ndim,x.shape)
-print(len(x.shape))
-
-x = np.array([[1,2,3],[4,5,6]])
-print (x,x.ndim,x.shape)
-print(len(x.shape))
-
-x = x.reshape((1,6))
-print (x,x.ndim,x.shape)
-print(len(x.shape))
-
-x = x.reshape((6))
-print (x,x.ndim,x.shape)
-print(len(x.shape))
-
-digit = np.array([[0,0,0],[0,0.5,1],[0,0,0]])
 import matplotlib.pyplot as plt
-plt.imshow(digit, cmap=plt.cm.binary)
-# plt.show()
-#----------------------------------------
+
+# make array, print ndim, shape
+x = np.array(12)
+print(x, x.ndim, x.shape)
+print(len(x.shape))
+
+x = np.array([1, 2, 3])
+print(x, x.ndim, x.shape)
+print(len(x.shape))
+
+x = np.array([[1, 2, 3], [4, 5, 6]])
+print(x, x.ndim, x.shape)
+print(len(x.shape))
+
+# using reshape
+x = x.reshape((1, 6))
+print(x, x.ndim, x.shape)
+print(len(x.shape))
+
+x = x.reshape(6)
+print(x, x.ndim, x.shape)
+print(len(x.shape))
+
+# using matplot binary
+digit = np.array([[0, 0, 8], [0, 5, 1], [100, 0, 2]])
+# plt.imshow(digit, cmap=plt.cm.binary)
+plt.imshow(digit)
+plt.show()
+#---------  -------------------------------
 x = np.array([[[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[2.1, 2.2, 2.3], [2.4, 2.5, 2.6], [2.7, 2.8, 2.9]], [[3, 3, 3], [3, 3, 3], [3, 3, 3]], [[4, 4, 4], [4, 4, 4], [4, 4, 4]]])
-print (x.ndim, x.shape)
+print ("x:", x.ndim, x.shape)
 
+print("x[1]:", x[1])
+print("x[2]:", x[2])
 y = x[1:3]
-print (y, y.ndim, y.shape)
+print ("y:", y, y.ndim, y.shape)
 
-y = x[1:3, 0:2, 0:3]
-print (y, y.ndim, y.shape)
+y = x[1:3, 0:1, 0:2]
+print ("\nx[1:3, 0:1, 0:2]\n", y, y.ndim, y.shape)
 
 rn = np.random.random((2,3,2))
-print(rn)
+print("rn:", rn)
 
 x = np.array([[[1,1],[1,1]],[[5,2],[3,3]]])
 print (x)
 y = np.array([[2,2],[3,3]])
 print (y)
 z = np.maximum(x,y)
-print(z)
+print("z:",z)
 
 x = np.array([[1,2,3],[0,0,0]])
 y = np.array([4,2,4])
@@ -51,6 +57,13 @@ x[1] += y[1]
 print (x)
 
 x = np.array([1,1,1])
-y = np.array(2)
+y = np.array([[2],[2],[2]])
 z = np.dot(x,y)
 print (z)
+
+l = []
+l.append([1,2])
+l.append([3,4])
+l.append([5,6])
+x = np.array(l)
+print(x)
